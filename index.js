922c11f60d3bfd3aa513e5e516313e0c68c9bb94
vaughnjones-marcus <vaughnjones.marcus@gmail.com>
2023-12-07 20:46:33 -0600

    Make flipped image appear on the card

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ fetch("./data/cards.json")
     .then((res) => res.json())
     .then((data) => {
         cards = [...data, ...data];
-        shufftleCards();
+        shuffleCards();
         generateCards();
     });
 
 function shuffleCards() {
-    let currentIndex = cards.lenght,
+    let currentIndex = cards.length,
         randomIndex,
         temporaryValue;
     while (currentIndex !== 0) {
@@ -28,5 +28,71 @@ function shuffleCards() {
 }
 
 function generateCards() {
-    
+    for (let card of cards) {
+        const cardElement = document.createElement("div");
+        cardElement.classList.add("card");
+        cardElement.setAttribute("data-name", card.name);
+        cardElement.innerHTML = `
+            <div class="front">
+                <img class="front-image" src="${card.image}" />
+            </div>
+            <div class="back"></div>
+            `;
+            gridContainer.appendChild(cardElement);
+            cardElement.addEventListener("click", flipCard);
+    }
+}
+
+function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
+
+    this.classList.add("flipped");
+
+    if (!firstCard) {
+        firstCard = this;
+        return;
+    }
+    secondCard = this;
+    score++;
+    document.querySelector(".score").textContent = score;
+    lockBoard = true;
+
+    checkForMatch();
+}
+
+function checkForMatch() {
+    let isMatch = firstCard.dataset.name === secondCard.dataset.name;
+
+    isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+    firstCard.removeEventListener("click", flipCard);
+    secondCard.removeEventListener("click", flipCard);
+
+    resetBoard();
+}
+
+function unflipCards() {
+    setTimeout(() => {
+        firstCard.classList.remove("flipped");
+        secondCard.classList.remove("flipped");
+        resetBoard();
+    }, 1000);
+}
+
+function resetBoard() {
+    firstCard = null;
+    secondCard = null;
+    lockBoard = false;
+}
+
+function restart() {
+    resetBoard();
+    shuffleCards();
+    score = 0;
+    document.querySelector(".score").textContent = score;
+    gridContainer.innerHTML = "";
+    generateCards();
 }
